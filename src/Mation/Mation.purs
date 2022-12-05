47138a8b364e3df9bc06dcb289c41ba60925cd66
Maynard <me@maynards.site>
2022-12-04 17:56:24 -0800

    remove some redundant parens

--- src/Mation/Mation.purs
+++ src/Mation/Mation.purs
@@ -12,10 +12,10 @@ mkCont :: forall m s. (Step s -> m Unit) -> Mation m s
 mkCont = Mation
 
 mkPure :: forall m s. MonadEffect m => (s -> s) -> Mation m s
-mkPure endo = Mation (\step -> liftEffect (step endo))
+mkPure endo = Mation \step -> liftEffect (step endo)
 
 mkEff :: forall m s. MonadEffect m => m (s -> s) -> Mation m s
-mkEff getEndo = Mation (\step -> getEndo >>= \endo -> liftEffect (step endo))
+mkEff getEndo = Mation \step -> getEndo >>= \endo -> liftEffect (step endo)
 
 runMation :: forall m s. Mation m s -> ((s -> s) -> Effect Unit) -> m Unit
 runMation (Mation f) step = f (step >>> liftEffect)
@@ -23,11 +23,11 @@ runMation (Mation f) step = f (step >>> liftEffect)
 -- FIXME: I think @Lens'@ can be weakened to @Setter'@
 embed :: forall m large small. Lens' large small -> Mation m small -> Mation m large
 embed lens (Mation f) =
-  Mation \apply -> f (\endo -> apply (lens %~ endo))
+  Mation \apply -> f \endo -> apply (lens %~ endo)
 
 instance Apply m => Semigroup (Mation m s) where
-  append (Mation f) (Mation g) = Mation (\step -> f step *> g step)
+  append (Mation f) (Mation g) = Mation \step -> f step *> g step
 
 instance Applicative m => Monoid (Mation m s) where
-  mempty = Mation (\_step -> pure unit)
+  mempty = Mation \_step -> pure unit
 
