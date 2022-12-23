16184c54cec84ff646aeb180e0f73ade508da83e
Maynard <me@maynards.site>
2022-12-23 02:02:08 -0800

    partial Generic impl for UnsureEq

--- src/Mation/Core/Util/UnsureEq.purs
+++ src/Mation/Core/Util/UnsureEq.purs
@@ -9,6 +9,9 @@ module Mation.Core.Util.UnsureEq
   , viaEq
   , viaPrim
   , primEq
+  , class UnsureEqGeRep
+  , gUnsureEq
+  , genericUnsureEq
   ) where
 
 import Mation.Core.Prelude
@@ -19,6 +22,7 @@ import Data.Symbol (class IsSymbol, reflectSymbol)
 import Prim.Row (class Nub) as R
 import Record.Unsafe (unsafeGet) as R
 import Prim.RowList as RL
+import Data.Generic.Rep as G
 
 
 data Unsure a = Unsure | Surely a
@@ -56,6 +60,20 @@ surely = case _ of
 -- | The canonical example is function types, where if two functions
 -- | are identical as references, we know they must be equivalent, but
 -- | otherwise we know nothing.
+-- |
+-- | ***
+-- |
+-- | The Mation framework uses UnsureEq to perform certain kinds of
+-- | caching. It can compare the new and old cache keys using `unsureEq`
+-- | and will recompute the cache value if the result is anything
+-- | other than `Surely true`. The benefit of choosing to use `UnsureEq`
+-- | for this instead of `Eq` is that we are able to perform a "best
+-- | effort" caching on types which *sometimes* has decidable equality,
+-- | such as
+-- |
+-- | ```
+-- | data IntOperation = Add Int | Subtract Int | SomethingElse (Int -> Int)
+-- | ```
 class UnsureEq a where
   unsureEq :: a -> a -> Unsure Boolean
 
@@ -122,6 +140,35 @@ else instance (IsSymbol lbl, UnsureEq head, UnsureEqFields tail rows) => UnsureE
         head1 = (R.unsafeGet lbl rec1 :: head)
         head2 = (R.unsafeGet lbl rec2 :: head)
     in unsureEq head1 head2 `min` unsureEqFields (Proxy :: Proxy tail) rec1 rec2
+
+
+-- The strategy for implementing `UnsureEq` on `Generic` types is essentially copied from [2]
+-- [1]: <https://github.com/purescript/purescript-prelude/blob/f4cad0ae8106185c9ab407f43cf9abf05c256af4/src/Data/Eq/Generic.purs>
+genericUnsureEq :: forall a rep. Generic a rep => UnsureEqGeRep rep => (a -> a -> Unsure Boolean)
+genericUnsureEq a a' = gUnsureEq (G.from a) (G.from a')
+
+class UnsureEqGeRep a where
+  gUnsureEq :: a -> a -> Unsure Boolean
+
+instance UnsureEqGeRep G.NoConstructors where
+  gUnsureEq _ _ = Surely true
+
+instance UnsureEqGeRep G.NoArguments where
+  gUnsureEq _ _ = Surely true
+
+instance (UnsureEqGeRep a, UnsureEqGeRep b) => UnsureEqGeRep (G.Sum a b) where
+  gUnsureEq (G.Inl a) (G.Inl a') = gUnsureEq a a'
+  gUnsureEq (G.Inr b) (G.Inr b') = gUnsureEq b b'
+  gUnsureEq _ _ = Surely false
+
+instance (UnsureEqGeRep a, UnsureEqGeRep b) => UnsureEqGeRep (G.Product a b) where
+  gUnsureEq (G.Product a b) (G.Product a' b') = gUnsureEq a a' `min` gUnsureEq b b'
+
+instance UnsureEqGeRep a => UnsureEqGeRep (G.Constructor name a) where
+  gUnsureEq = gUnsureEq `on` case _ of G.Constructor a -> a
+
+instance UnsureEqGeRep a => UnsureEqGeRep (G.Argument a) where
+  gUnsureEq = gUnsureEq `on` case _ of G.Argument a -> a
 
 
 
