fbf1d7b7eb7b650c1a3b194a03e3e8a2e39630af
Maynard <me@maynards.site>
2025-12-14 21:47:57 -0800

    add regression test "PruneOverwrite"

---
+++ tests/PruneOverwrite.purs
@@ -0,0 +1,66 @@
+module Mation.Tests.PruneOverwrite where
+
+{- BEGIN METADATA
+
+{
+  "name": "Prune Overwrite",
+  "desc": "Tests a diffing bug",
+  "specs": [
+    "Clicking the button should cycle between (A) both \"non-pruned\" and \"pruned\" being shown, and (B) only \"pruned\" being shown"
+  ]
+}
+
+END METADATA -}
+
+
+{-
+
+BUG: If a non-pruned <span> is rendered where a pruned element
+just was, then diffing of the pruned element will break.
+
+The issue appears to be that the non-pruned <span> is wrongly
+diffing against the rendered <span style="display: contents"> of
+the pruned node.
+
+-}
+
+import Mation.Core.Prelude
+
+import Mation as M
+import Mation.Elems as E
+import Mation.Props as P
+import Mation.Lenses (field)
+import Mation.Core.Refs as Refs
+
+
+type Model =
+  { showSpan :: Boolean
+  }
+
+view :: Model -> E.Html' (M.Modify Model)
+view model =
+  E.div
+  [ P.addCss "font-family: sans-serif"
+  ]
+  [ E.p []
+    [ E.button
+      [ P.onClick \_ -> Refs.modify (field @"showSpan" %~ not) ]
+      [ E.text "Click me" ]
+    ]
+
+  , guard model.showSpan $ fold
+    [ E.span [] [ E.text "non-pruned" ]
+    , E.br [] `power` 2
+    ]
+  , flip (E.pruneEq "the-prune") unit \_ -> E.div [] [ E.text "pruned" ]
+  ]
+
+main :: Effect Unit
+main = do
+  M.runApp
+    { initial: { showSpan: true }
+    , render: view >>> cmap Refs.downcast
+    , root: M.underBody
+    , daemon: mempty
+    }
+
